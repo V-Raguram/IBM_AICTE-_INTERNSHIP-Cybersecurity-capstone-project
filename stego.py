fff8e2578355061d17ae0f8c06d41324b966e110
PurpleBolt <145138895+V-Raguram@users.noreply.github.com>
2025-02-26 20:21:02 +0530

    Update stego.py

--- stego.py
+++ stego.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import string
 
-img = cv2.imread("mypic.jpg") # Replace with the correct image path#image
+img = cv2.imread("mypic.jpg") 
 
 msg = input("Enter secret message:")
 password = input("Enter a passcode:")
@@ -25,7 +25,7 @@ for i in range(len(msg)):
     z = (z + 1) % 3
 
 cv2.imwrite("encryptedImage.jpg", img)
-os.system("start encryptedImage.jpg")  # Use 'start' to open the image on Windows
+os.system("start encryptedImage.jpg")  
 
 message = ""
 n = 0
